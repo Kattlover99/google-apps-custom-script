5b976dbd8b38d329e5c5429a93c50fee6afab50d
kattlover99 <thrillseeker.nw@gmail.com>
2023-08-29 21:57:44 +0300

    Add: distanceBetweenPoints func

--- function.js
+++ function.js
@@ -16,3 +16,17 @@ function clearInvoice() {
     const invoiceTo = sheet.getRange("E5").clearContent();
     const invoiceFrom = sheet.getRange("E6").clearContent();
 }
+
+function distanceBetweenPoints(start_point, end_point) {
+    // get the directions
+    const directions = Maps.newDirectionFinder()
+        .setOrigin(start_point)
+        .setDestination(end_point)
+        .setMode(Maps.DirectionFinder.Mode.DRIVING)
+        .getDirections();
+
+    // get the first route and return the distance
+    const route = directions.routes[0];
+    const distance = route.legs[0].distance.text;
+    return distance;
+}
