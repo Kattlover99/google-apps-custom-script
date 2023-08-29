b8664e299be966d72b6e72253b5a1a37eceddf28
kattlover99 <thrillseeker.nw@gmail.com>
2023-08-29 21:55:53 +0300

    Add: clearInvoice func

--- function.js
+++ function.js
@@ -8,3 +8,11 @@ function onOpen() {
 function helloWorld() {
     Browser.msgBox("Hello World!");
 }
+
+function clearInvoice() {
+    const sheet = SpreadsheetApp.getActiveSheet();
+    const invoiceNumber = sheet.getRange("B5").clearContent();
+    const invoiceAmount = sheet.getRange("B8").clearContent();
+    const invoiceTo = sheet.getRange("E5").clearContent();
+    const invoiceFrom = sheet.getRange("E6").clearContent();
+}
