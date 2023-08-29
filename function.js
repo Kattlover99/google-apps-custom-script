203b8b60df40a02efdba7ef4422f69d40150881d
kattlover99 <thrillseeker.nw@gmail.com>
2023-08-29 22:02:35 +0300

    Add:  formulasToValuesGlobal

--- function.js
+++ function.js
@@ -70,3 +70,12 @@ function formulasToValuesActiveSheet() {
     var range = sheet.getDataRange();
     range.copyValuesToRange(sheet, 1, range.getLastColumn(), 1, range.getLastRow());
 };
+
+// convert all formulas to values in every sheet of the Google Sheet
+function formulasToValuesGlobal() {
+    var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
+    sheets.forEach(function (sheet) {
+        var range = sheet.getDataRange();
+        range.copyValuesToRange(sheet, 1, range.getLastColumn(), 1, range.getLastRow());
+    });
+};
